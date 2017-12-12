68269f3ce95ac8ea76dd5597c2ca7a574985a4ff
kavdihodgson <kavdyjh@gmail.com>
2017-12-12 14:04:10 -0800

    finished insertion sort algorithm. need to do testing.

--- src/insertion.py
+++ src/insertion.py
@@ -4,11 +4,11 @@
 def insertion_sort(arr):
     """Sort from lowest to highest with insertion method."""
     l = len(arr)
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     for i in range(1, l):
-        x = i
-        while x > 0:
-            if arr[x] < arr[x - 1]:
-                arr[x - 1], arr[x] = arr[x], arr[x - 1]
-                x = x - 1
+        while i - 1 != -1:
+            if arr[i] < arr[i - 1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+            if i != 0:
+                i = i - 1
     return arr
