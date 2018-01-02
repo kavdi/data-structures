f71e19744dfc930a404ca4fcb4c37b8e15f92636
kavdihodgson <kavdyjh@gmail.com>
2018-01-02 14:28:59 -0800

    added tests for hash table.

--- src/test_hash.py
+++ src/test_hash.py
@@ -30,6 +30,64 @@ def test_horner_hash_returns_correct_hash_values_for_strings_given(key, value):
     assert horner_hash(key) == value
 
 
-def test_Hahs_Table_uses_provided_hash_function():
+def test_hash_table_constructor_creates_empty_hash_table():
+    """Test that HashTable constructor creates an empty hash table."""
+    from hash import additive_hash, HashTable
+    t = HashTable(10, additive_hash)
+    assert len(t.buckets) == 10
+    assert t.hash_func is additive_hash
+    assert not all(t.buckets)
+
+
+def test_hash_table_uses_additive_hash_function():
     """Test that hash table hashes keys with function provided."""
-    
+    from hash import additive_hash, HashTable
+    t = HashTable(10, additive_hash)
+    assert t._hash('one') == additive_hash('one')
+
+
+def test_hash_table_uses_horner_hash_function():
+    """Test that hash table hashes keys with function provided."""
+    from hash import horner_hash, HashTable
+    t = HashTable(10, horner_hash)
+    assert t._hash('two') == horner_hash('two')
+
+
+def test_set_raises_error_if_key_not_string():
+    """Test that type error raised if key not string in set."""
+    from hash import HashTable, additive_hash
+    t = HashTable(10, additive_hash)
+    with pytest.raises(TypeError):
+        t.set(12, 44)
+
+
+def test_set_places_key_value_pairs_in_hash_table():
+    """Test that set function places key value pairs in hash table correctly."""
+    from hash import HashTable, additive_hash
+    t = HashTable(10, additive_hash)
+    t.set('word', 444)
+    assert t.buckets[4][0] == ['word', 444]
+
+
+def test_get_returns_value_of_key_provided():
+    """Test that get function returns the value of key provided."""
+    from hash import HashTable, additive_hash
+    t = HashTable(10, additive_hash)
+    t.set('word', 444)
+    assert t.get('word')
+
+
+def test_get_raises_error_if_key_not_not_in_table():
+    """Test that keyerror raised if key not in table."""
+    from hash import HashTable, additive_hash
+    t = HashTable(10, additive_hash)
+    with pytest.raises(KeyError):
+        t.get('key')
+
+
+def test_get_non_string_key_raises_error():
+    """Test taht get with a non string key raises error."""
+    from hash import HashTable, additive_hash
+    t = HashTable(10, additive_hash)
+    with pytest.raises(TypeError):
+        t.get(22)
