283af70db343dc95931aa515d703f7e9e2f8fe47
kavdihodgson <kavdyjh@gmail.com>
2018-01-03 14:03:27 -0800

    insertion sort timeit finished.

--- src/insertion.py
+++ src/insertion.py
@@ -1,4 +1,6 @@
 """Implement insertion sort algorithm."""
+from random import randint, shuffle
+from timeit import timeit
 
 
 def insertion_sort(arr):
@@ -11,3 +13,40 @@ def insertion_sort(arr):
             if i != 0:
                 i = i - 1
     return arr
+
+
+def timings():  # pragma: no cover
+    """Generate timings report for insertion sort."""
+    import_sort = 'from insertion import insertion_sort'
+    print("""
+    Timings for best, average and worst case scenarios for the insertion sort.
+    --------------------------------------------------------------------------
+    """)
+
+    print("3 Best Case Scenarios - sorted except for one value")
+    for i in range(3):
+        lst_len = randint(9, 50)
+        rand_lst = [i for i in range(lst_len)]
+        rand_lst[6], rand_lst[-1] = rand_lst[-1], rand_lst[6]
+        best_time = timeit('insertion_sort({})'.format(rand_lst), import_sort)
+        print('List {}: length={}; time = {}'.format(i + 1, lst_len, best_time))
+
+    print("\n3 Average Case Scenarios - Moderately sorted")
+    for i in range(3):
+        lst_len = randint(9, 50)
+        rand_lst = [i for i in range(lst_len)]
+        shuffle(rand_lst)
+        best_time = timeit('insertion_sort({})'.format(rand_lst), import_sort)
+        print('List {}: length={}; time = {}'.format(i + 1, lst_len, best_time))
+
+    print("\n3 Worst Case Scenarios - Completely unsorted")
+    for i in range(3):
+        lst_len = randint(9, 50)
+        rand_lst = [i for i in range(lst_len)]
+        rand_lst = rand_lst[::-1]
+        best_time = timeit('insertion_sort({})'.format(rand_lst), import_sort)
+        print('List {}: length={}; time = {}'.format(i + 1, lst_len, best_time))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    timings()
