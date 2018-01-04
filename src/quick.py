6bf30f0be8f27b0416b2b2ac238dd93869ce45f6
kavdihodgson <kavdyjh@gmail.com>
2018-01-03 23:55:02 -0800

    finished quicksort.

--- src/quick.py
+++ src/quick.py
@@ -15,3 +15,22 @@ def quick_sort(arr):
                 arr[left], arr[right] = arr[right], arr[left]
                 left += 1
                 right -= 1
+
+            elif arr[left] <= pivot and arr[right] < pivot:
+                left += 1
+
+            elif arr[left] > pivot and arr[right] >= pivot:
+                right -= 1
+
+            elif arr[left] <= pivot and arr[right] >= pivot:
+                left += 1
+                right -= 1
+
+        arr[0], arr[right] = arr[right], arr[0]
+
+        divider = right + 1
+        first = quick_sort(arr[:right])
+        second = quick_sort(arr[divider:])
+        return first + [arr[right]] + second
+    else:
+        return arr
