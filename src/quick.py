de753b06f7b67566c5f787d7e1ec3e05bf736bd6
kavdihodgson <kavdyjh@gmail.com>
2018-01-04 18:03:05 -0800

    added timeit to quick-sort.

--- src/quick.py
+++ src/quick.py
@@ -1,4 +1,6 @@
 """Implement quick sort algorithm."""
+from random import randint, shuffle
+from timeit import timeit
 
 
 def quick_sort(arr):
@@ -34,3 +36,40 @@ def quick_sort(arr):
         return first + [arr[right]] + second
     else:
         return arr
+
+
+def timings():  # pragma: no cover
+    """Generate timings report for insertion sort."""
+    import_sort = 'from quick import quick_sort'
+    print("""
+    Timings for best, average and worst case scenarios for the merge sort.
+    --------------------------------------------------------------------------
+    """)
+
+    print("3 Best Case Scenarios - sorted except for one value")
+    for i in range(3):
+        lst_len = randint(9, 50)
+        rand_lst = [i for i in range(lst_len)]
+        rand_lst[6], rand_lst[-1] = rand_lst[-1], rand_lst[6]
+        best_time = timeit('quick_sort({})'.format(rand_lst), import_sort)
+        print('List {}: length={}; time = {}'.format(i + 1, lst_len, best_time))
+
+    print("\n3 Average Case Scenarios - Moderately sorted")
+    for i in range(3):
+        lst_len = randint(9, 50)
+        rand_lst = [i for i in range(lst_len)]
+        shuffle(rand_lst)
+        best_time = timeit('quick_sort({})'.format(rand_lst), import_sort)
+        print('List {}: length={}; time = {}'.format(i + 1, lst_len, best_time))
+
+    print("\n3 Worst Case Scenarios - Completely unsorted")
+    for i in range(3):
+        lst_len = randint(9, 50)
+        rand_lst = [i for i in range(lst_len)]
+        rand_lst = rand_lst[::-1]
+        best_time = timeit('quick_sort({})'.format(rand_lst), import_sort)
+        print('List {}: length={}; time = {}'.format(i + 1, lst_len, best_time))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    timings()
